f7a750ef50b62713cb4caddf2e71d5feb8bfefea
karliuka <karliuka.vitalii@gmail.com>
2019-08-17 14:38:53 +0300

    update registration

--- registration.php
+++ registration.php
@@ -3,8 +3,10 @@
  * Copyright © Karliuka Vitalii(karliuka.vitalii@gmail.com)
  * See COPYING.txt for license details.
  */
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
     'Faonni_Price',
     __DIR__
 );
