2983f74b0658ad65dc60fb3b3e5874ce1f4faee4
karliuka <karliuka.vitalii@gmail.com>
2020-12-21 22:33:54 +0200

    add simple round processors

---
+++ Model/Calculator/RoundProcessor/Simple/FloorProcessor.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright © Karliuka Vitalii(karliuka.vitalii@gmail.com)
+ * See COPYING.txt for license details.
+ */
+namespace Faonni\Price\Model\Calculator\RoundProcessor\Simple;
+
+use Faonni\Price\Model\Calculator\RoundProcessorInterface;
+
+/**
+ * Simple floor round processor
+ */
+class FloorProcessor implements RoundProcessorInterface
+{
+    /**
+     * Retrieve the rounded price
+     *
+     * @param float $price
+     * @return float
+     */
+    public function round($price)
+    {
+        return floor($price);
+    }
+}
